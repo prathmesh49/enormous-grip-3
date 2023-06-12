e05fce4d7a3befdd2aff6dad74fb8a9e901dfb1f
prathmesh49 <prathmesh_kurane@yahoo.com>
2023-06-12 22:36:48 +0530

    initial

---
+++ src/test/java/com/amusement/AmusementParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amusement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmusementParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
