0a3b2a719d9b5b567bcb9dd28d216f967843a30c
prathmesh49 <prathmesh_kurane@yahoo.com>
2023-06-12 22:53:34 +0530

    initial

---
+++ AmusementParkManagementSystem/src/main/java/com/amusement/AmusementParkApplication.java
@@ -0,0 +1,13 @@
+package com.amusement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmusementParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmusementParkApplication.class, args);
+	}
+
+}
