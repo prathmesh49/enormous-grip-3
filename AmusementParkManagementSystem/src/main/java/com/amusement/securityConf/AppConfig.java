f264b4b98cd5477739c3e8d1a14a003da3da9337
VikashBhuyan <vikashbhuyan2000@gmail.com>
2023-06-19 14:55:31 +0530

    Documentation

--- AmusementParkManagementSystem/src/main/java/com/amusement/securityConf/AppConfig.java
+++ AmusementParkManagementSystem/src/main/java/com/amusement/securityConf/AppConfig.java
@@ -2,6 +2,7 @@ package com.amusement.securityConf;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,7 +16,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.Collections;
-
+@Configuration
 public class AppConfig {
 
 
